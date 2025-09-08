2fb1dcb8f92e7bb7cd4edd257dc909e9be7780f6
Junbeom Park <ephesi4ns623@gmail.com>
2025-09-08 22:25:22 +0900

    fix: query with Status complete

--- app/crud.py
+++ app/crud.py
@@ -1,13 +1,17 @@
 from typing import List
 from sqlalchemy.orm import Session
 
-from app.models import NotionPage
+from app.models import NotionPage, Status
 
 
 def list_posts(session: Session) -> List[NotionPage]:
     return (
         session.query(NotionPage)
-        .filter(NotionPage.is_deleted.is_(False))
+        .join(Status, NotionPage.status_id == Status.id)
+        .filter(
+            NotionPage.is_deleted.is_(False),
+            Status.name == "완료",
+        )
         .order_by(NotionPage.created_time.desc())
         .all()
     )
